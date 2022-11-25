44d8b639632c1692754e641b673260ab37e3510c
Danilo Martins <dansm86@outlook.com>
2022-11-25 01:26:41 -0300

    feat:added index.ts to interfaces folder

---
+++ app/backend/src/utils/interfaces/index.ts
@@ -0,0 +1,11 @@
+import IController from './controller.interface';
+import ILoginData from './login.data.interface';
+import ILoginService from './user.service.interface';
+import IToken from './token.interface';
+
+export {
+  IController,
+  ILoginData,
+  ILoginService,
+  IToken,
+};
