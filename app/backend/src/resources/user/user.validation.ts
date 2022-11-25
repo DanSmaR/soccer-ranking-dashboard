c3238906c4a14ce00d55b9818209693c6dfdb65e
Danilo Martins <dansm86@outlook.com>
2022-11-25 01:27:07 -0300

    feat:created a joi login schema

---
+++ app/backend/src/resources/user/user.validation.ts
@@ -0,0 +1,10 @@
+import * as Joi from 'joi';
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required,
+}).messages({
+  'any.required': 'All fields must be filled',
+});
+
+export default loginSchema;
