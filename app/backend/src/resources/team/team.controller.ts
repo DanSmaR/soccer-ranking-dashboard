3a3022807da4518983a8fd157c9791cf4da737dc
Danilo Martins <dansm86@outlook.com>
2022-11-27 12:29:37 -0300

    feat:sec2:created TeamController with getTeams
    method

---
+++ app/backend/src/resources/team/team.controller.ts
@@ -0,0 +1,29 @@
+import { Router, Request, Response, NextFunction, } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import HttpException from '../../utils/exceptions/http.exception';
+import { IController } from '../../utils/interfaces';
+import TeamSequelizeAdapter from './team.sequelize.model';
+import TeamService from './team.service';
+import ITeam from './team.interface';
+
+export default class TeamController implements IController {
+  public path = '/teams';
+  public router = Router();
+
+  constructor(private teamService: ITeamService = new TeamService()) {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes(): void {
+    this.router.get(this.path, this.getTeams);
+  }
+
+  private getTeams = async (
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+  ): Promise<Response | void> => {
+    const teams = await this.teamService.getTeams();
+    res.status(200).json(teams);
+  };
+}
