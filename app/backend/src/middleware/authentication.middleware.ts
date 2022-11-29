7251f22d816c75fe70247b38ed4c32c08ae9697a
Danilo Martins <dansm86@outlook.com>
2022-11-29 00:02:05 -0300

    feat:sec3:invalid token when creating match
    refactored authentication middleware

--- app/backend/src/middleware/authentication.middleware.ts
+++ app/backend/src/middleware/authentication.middleware.ts
@@ -8,7 +8,7 @@ import HttpException from '../utils/exceptions/http.exception';
 function authenticateMiddleware(userModel: IUserModel): RequestHandler {
   return async (req: Request, _res: Response, next: NextFunction): Promise<Response | void> => {
     const token = req.headers.authorization;
-    if (!token) throw new HttpException(StatusCodes.UNAUTHORIZED, 'Invalid Token');
+    if (!token) throw new HttpException(StatusCodes.UNAUTHORIZED, 'Token must be a valid token');
     const payload = verifyToken(token);
     const user = await userModel.findById(payload.id);
     if (!user) {
--- app/backend/src/utils/token.ts
+++ app/backend/src/utils/token.ts
@@ -1,14 +1,13 @@
 import * as jwt from 'jsonwebtoken';
 import 'dotenv/config';
 import { StatusCodes } from 'http-status-codes';
-// import { IToken } from './interfaces';
 import IUser from '../resources/user/user.interface';
 import HttpException from './exceptions/http.exception';
 
 export const createToken = (user: IUser): string => jwt.sign(
   { id: user.id },
   process.env.JWT_SECRET as jwt.Secret,
-  { expiresIn: '1d', algorithm: 'HS256' },
+  { expiresIn: '5m', algorithm: 'HS256' },
 );
 
 export const verifyToken = (
@@ -21,6 +20,6 @@ export const verifyToken = (
     ) as jwt.JwtPayload;
     return payload;
   } catch (error) {
-    throw new HttpException(StatusCodes.UNAUTHORIZED, 'Invalid Token');
+    throw new HttpException(StatusCodes.UNAUTHORIZED, 'Token must be a valid token');
   }
 };
