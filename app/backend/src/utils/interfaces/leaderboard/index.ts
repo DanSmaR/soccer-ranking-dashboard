941f9ec4015ef04b35a18470190e4bec92308338
Danilo Martins <dansm86@outlook.com>
2022-11-30 10:41:04 -0300

    feat:sec4:index to export leaderboard interfaces

---
+++ app/backend/src/utils/interfaces/leaderboard/index.ts
@@ -0,0 +1,7 @@
+import ILeaderBoardModel from './leaderboard.model.interface';
+import ILeaderBoardService from './leaderboard.service.interface';
+
+export {
+  ILeaderBoardModel,
+  ILeaderBoardService,
+};
